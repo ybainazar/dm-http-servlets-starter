5bd2b90112be86846178c38f49a0371e3f8d3396
ybainazar <dom8152daleN!>
2022-05-22 21:42:02 +0600

    new

--- src/dmdev/http_servlets/server/HttpClientRunner.java
+++ src/dmdev/http_servlets/server/HttpClientRunner.java
@@ -7,9 +7,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
 
 public class HttpClientRunner {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         var httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
@@ -20,8 +21,11 @@ public class HttpClientRunner {
                 .POST(HttpRequest.BodyPublishers.ofFile(Path.of("resources", "first.json")))
                 .build();
 
-        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.headers());
-        System.out.println(response.body());
+        var response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response2 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response3 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.headers());
+//        System.out.println(response.body());
+        System.out.println(response3.get().body());
     }
 }
--- src/dmdev/http_servlets/server/HttpServerRunner.java
+++ src/dmdev/http_servlets/server/HttpServerRunner.java
@@ -2,7 +2,7 @@ package dmdev.http_servlets.server;
 
 public class HttpServerRunner {
     public static void main(String[] args) {
-        var httpServer = new HttpServer(9000);
+        var httpServer = new HttpServer(100, 9000);
         httpServer.run();
     }
 }
