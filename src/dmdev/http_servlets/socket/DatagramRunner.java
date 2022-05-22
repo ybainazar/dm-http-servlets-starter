2620d5b58dc3e30efd88e7ebebcfa655e8484583
ybainazar <dom8152daleN!>
2022-05-22 18:33:58 +0600

    new

---
+++ src/dmdev/http_servlets/socket/DatagramRunner.java
@@ -0,0 +1,15 @@
+package dmdev.http_servlets.socket;
+
+import java.io.IOException;
+import java.net.*;
+
+public class DatagramRunner {
+    public static void main(String[] args) throws IOException {
+        var inetAddress = InetAddress.getByName("localhost");
+        try (var datagramSocket = new DatagramSocket()) {
+            var bytes = "Hello from UDP client".getBytes();
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, inetAddress, 7777);
+            datagramSocket.send(packet);
+        }
+    }
+}
