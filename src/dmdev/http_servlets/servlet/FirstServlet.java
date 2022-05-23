6e079a8e6df90c59713830e819f518f6d5bdddc3
ybainazar <dom8152daleN!>
2022-05-23 14:54:12 +0600

    new

---
+++ src/dmdev/http_servlets/servlet/FirstServlet.java
@@ -0,0 +1,35 @@
+package dmdev.http_servlets.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (var writer = resp.getWriter()) {
+            writer.write("<h1>Hello from First Servlet</h2>");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+
+}
