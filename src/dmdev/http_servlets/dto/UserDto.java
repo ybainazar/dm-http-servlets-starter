e64aab8a1cf29fd0e8765124fff2851ca932d78f
ybainazar <dom8152daleN!>
2022-05-25 14:36:43 +0600

    new

---
+++ src/dmdev/http_servlets/dto/UserDto.java
@@ -0,0 +1,11 @@
+package dmdev.http_servlets.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class UserDto {
+    Long id;
+    String mail;
+}
--- src/dmdev/http_servlets/servlet/SessionServlet.java
+++ src/dmdev/http_servlets/servlet/SessionServlet.java
@@ -1,5 +1,6 @@
 package dmdev.http_servlets.servlet;
 
+import dmdev.http_servlets.dto.UserDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,9 +13,18 @@ import java.io.IOException;
 @WebServlet("/sessions")
 public class SessionServlet extends HttpServlet {
 
+    public static final String USER = "user";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var session = req.getSession();
-        System.out.println(session.isNew());
+        var user = (UserDto) session.getAttribute("user");
+        if (user == null) {
+            user = UserDto.builder()
+                    .id(25L)
+                    .mail("test@mail.com")
+                    .build();
+            session.setAttribute(USER, user);
+        }
     }
 }
