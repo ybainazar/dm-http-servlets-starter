a30a0a4f14421c0752b46bd756a2f70c1e76f865
ybainazar <dom8152daleN!>
2022-05-25 16:19:22 +0600

    new

--- src/dmdev/http_servlets/servlet/ContentServlet.java
+++ src/dmdev/http_servlets/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package dmdev.http_servlets.servlet;
 
+import dmdev.http_servlets.dto.FlightDto;
+import dmdev.http_servlets.service.FlightService;
 import dmdev.http_servlets.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,13 +11,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import static java.util.stream.Collectors.*;
+
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
-
+    private final FlightService flightService = FlightService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -7,7 +7,16 @@
 <body>
     <%@ include file="header.jsp"%>
     <div>
-        Content. Русский
+        <span>Content. Русский</span>
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights.get(0).id}</p>
+        <p>Id 2: ${requestScope.flights[1].id}</p> <%--null safe--%>
+        <p>Map 2: ${sessionScope.flightsMap[1]}</p>
+        <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+        <p>Empty list: ${not empty requestScope.flights}</p>
     </div>
     <%@ include file="footer.jsp"%>
 </body>
