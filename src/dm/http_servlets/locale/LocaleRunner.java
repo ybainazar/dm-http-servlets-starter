fbbddfbfc95b76ba41effb50107b13baf1c5f42f
ybainazar <dom8152daleN!>
2022-05-27 20:54:17 +0600

    new

---
+++ src/dm/http_servlets/locale/LocaleRunner.java
@@ -0,0 +1,17 @@
+package dm.http_servlets.locale;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LocaleRunner {
+    public static void main(String[] args) {
+        Locale locale = new Locale("ru", "RU");
+        Locale localeEn = new Locale("en", "EN");
+//        Locale.US
+        System.out.println(Locale.US);
+        System.out.println(Locale.getDefault());
+
+        var translations = ResourceBundle.getBundle("translations");
+        System.out.println(translations.getString("page.login.password"));
+    }
+}
